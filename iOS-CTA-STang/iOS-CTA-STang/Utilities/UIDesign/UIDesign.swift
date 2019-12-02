c205083fa335635cc46d8df45129872e578b3baa
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-02 12:05:19 -0500

    creates UIDesign file

---
+++ iOS-CTA-STang/iOS-CTA-STang/Utilities/UIDesign/UIDesign.swift
@@ -0,0 +1,38 @@
+//
+//  UIDesign.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/2/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+struct UIDesign {
+    static func styleTextField(_ textField: UITextField) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
+        bottomLine.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        textField.borderStyle = .roundedRect
+        textField.layer.addSublayer(bottomLine)
+    }
+    
+    static func styleButton(_ button: UIButton) {
+        button.backgroundColor = .clear
+        button.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        button.layer.borderWidth = 3
+        button.layer.cornerRadius = 15.0
+        button.tintColor = UIColor.white
+        button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 15)
+        button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        button.showsTouchWhenHighlighted = true
+    }
+    
+    static func styleButtonPressed(_ button: UIButton) {
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 25.0
+        button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 15)
+        button.tintColor = UIColor.black
+    }
+}
