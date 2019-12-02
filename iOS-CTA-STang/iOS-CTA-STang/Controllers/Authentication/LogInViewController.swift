8d1aac0f519b50f744344def0dd2d870048b1ff0
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-02 12:05:49 -0500

    adds UI objects to LogInVC

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/Authentication/LogInViewController.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/Authentication/LogInViewController.swift
@@ -9,25 +9,75 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    // MARK: - UI Objects
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 30)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Email"
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Password"
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        //        button.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var logInStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    lazy var createAccountButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create New Account", for: .normal)
+        button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 15)
+        button.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        button.showsTouchWhenHighlighted = true
+        //        button.addTarget(self, action: #selector(createAccountButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        
+        addSubviews()
+        addConstraints()
+        styleObjects()
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
