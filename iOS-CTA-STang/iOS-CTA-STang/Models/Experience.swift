3984ec347f31fc362b7bcb9d3bea5076690d0274
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-02 15:37:05 -0500

    Creates Experience enum

---
+++ iOS-CTA-STang/iOS-CTA-STang/Models/Experience.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/2/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+enum Experience: String {
+    case ticketMaster = "Ticketmaster"
+    case rijksmuseum = "Rijksmuseum"
+}
