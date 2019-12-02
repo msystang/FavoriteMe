ba74e1f633587a011bda5abbada1345eda6faf42
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-02 11:07:04 -0500

    creates empty AppUser file

---
+++ iOS-CTA-STang/iOS-CTA-STang/Models/MuseumItem.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/2/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    
+}
---
+++ iOS-CTA-STang/iOS-CTA-STang/Views/ObjectTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ObjectTableViewCell.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/2/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class ObjectTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
