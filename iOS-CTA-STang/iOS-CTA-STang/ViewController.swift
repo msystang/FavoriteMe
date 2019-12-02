7ee140a1d5bd56840ebe879e2e862fea04827c8d
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-02 10:40:23 -0500

    initial commit

---
+++ iOS-CTA-STang/iOS-CTA-STang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/2/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
