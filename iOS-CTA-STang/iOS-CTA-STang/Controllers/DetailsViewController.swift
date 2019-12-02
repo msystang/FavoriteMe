2cb5ceae9d6052d472f28a46e12aa77e4bb69c20
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-02 14:44:10 -0500

    adds objects and constraints to DetailsVC in storyboard

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/DetailsViewController.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/DetailsViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        /* TODO: Presenting DetailsVC modally in didSelect in tableview.
+         let storyboard = UIStoryboard(name: "Main", bundle: nil)
+         let controller = storyboard.instantiateViewController(withIdentifier: "DetailsViewController")
+         self.present(controller, animated: true, completion: nil)
+         */
     }
-    */
-
+    
+    
 }
