3ed26490259f0933bedd802a5c98bac4d6b7815b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 15:06:41 -0500

    adds tableview delegate and datasource placeholders in SearchListVC

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/Search/SearchListViewController+TableView.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/Search/SearchListViewController+TableView.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension SearchListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension SearchListViewController: UITableViewDelegate {
+    
+}
