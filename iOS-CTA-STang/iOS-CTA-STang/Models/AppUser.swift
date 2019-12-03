f38fc855af6eb06390d85391808086699e273692
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 14:11:43 -0500

    creates AppUser model

--- iOS-CTA-STang/iOS-CTA-STang/Models/AppUser.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Models/AppUser.swift
@@ -7,7 +7,39 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
 
 struct AppUser {
+    let email: String?
+    let uid: String
+    let selectedExperience: String?
+    let dateCreated: Date?
     
+    //Encoding to Firestore
+    init(from user: User, selectedExperience: UserExperience) {
+        self.email = user.email
+        self.uid = user.uid
+        self.selectedExperience = selectedExperience.rawValue
+        self.dateCreated = user.metadata.creationDate
+    }
+    
+    //Decoding to Firestore
+    init?(from dict: [String: Any], id: String) {
+        guard let email = dict["email"] as? String,
+            let selectedExperience = dict["selectedExperience"] as? String,
+            let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue() else { return nil }
+        
+        self.uid = id
+        self.email = email
+        self.selectedExperience = selectedExperience
+        self.dateCreated = dateCreated
+    }
+    
+    var fieldsDict: [String: Any] {
+        return [
+            "email": self.email ?? "",
+            "selectedExperience": self.selectedExperience ?? UserExperience.ticketMaster.rawValue
+        ]
+    }
 }
