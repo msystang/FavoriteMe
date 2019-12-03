801e312fe2b708c6c3aed4ab75926d85b678e31b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 14:11:56 -0500

    creates Favorite model

--- iOS-CTA-STang/iOS-CTA-STang/Models/Favorite.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Models/Favorite.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct Favorite {
+    let id: String
+    let creatorID: String
+    let title: String
+    let details: String
+    let isFavorite: Bool
+    let dateCreated: Date?
+    
+    //Encoding to Firestore
+    init(creatorID: String, title: String, details: String, isFavorite: Bool = true, dateCreated: Date? = nil) {
+        self.id = UUID().description
+        self.creatorID = creatorID
+        self.title = title
+        self.details = details
+        self.isFavorite = isFavorite
+        self.dateCreated = dateCreated
+    }
+    
+    //Decoding from Firestore
+    init?(from dict: [String: Any], id: String) {
+        guard let userID = dict["creatorID"] as? String,
+        let title = dict["title"] as? String,
+        let details = dict["details"] as? String,
+        let isFavorite = dict["isFavorite"] as? Bool else { return nil }
+        let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue()
+        
+        self.id = id
+        self.creatorID = userID
+        self.title = title
+        self.details = details
+        self.isFavorite = isFavorite
+        self.dateCreated = dateCreated
+    }
+    
+    var fieldsDict: [String: Any] {
+        return [
+            "creatorID": self.creatorID,
+            "title": self.title,
+            "details": self.details,
+            "isFavorite": self.isFavorite
+        ]
+    }
+    
+    // Instantiating Favorite from Event
+    init(creatorID: String, event: Event, isFavorite: Bool = true, dateCreated: Date? = nil) {
+        self.id = UUID().description
+        self.creatorID = creatorID
+        self.title = event.name
+        self.details = event.dates.start.formattedDate
+        self.isFavorite = isFavorite
+        self.dateCreated = dateCreated
+    }
+    
+    // Instantiating Favorite from MuseumItem
+    init(creatorID: String, museumItem: MuseumItem, isFavorite: Bool = true, dateCreated: Date? = nil) {
+        self.id = UUID().description
+        self.creatorID = creatorID
+        self.title = museumItem.title
+        self.details = museumItem.principalOrFirstMaker
+        self.isFavorite = isFavorite
+        self.dateCreated = dateCreated
+    }
+    
+}
