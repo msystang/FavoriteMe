f8d1b185dafb63a727454734856b9fa2e02759d1
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 16:21:18 -0500

    loads title in SearchListVC TableView extension

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/Search/SearchListViewController+TableView.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/Search/SearchListViewController+TableView.swift
@@ -10,16 +10,47 @@ import UIKit
 
 extension SearchListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if selectedExperience != nil {
+            switch selectedExperience! {
+            case UserExperience.ticketMaster:
+                return events.count
+            case UserExperience.rijksmuseum:
+                return museumItems.count
+            }
+        }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        
+        if selectedExperience != nil {
+            switch selectedExperience! {
+                
+            case UserExperience.ticketMaster:
+                let event = events[indexPath.row]
+                
+                cell.titleLabel.text = event.name
+                
+            case UserExperience.rijksmuseum:
+                let museumItem = museumItems[indexPath.row]
+                
+                cell.titleLabel.text = museumItem.title
+            }
+        }
+        
+        return cell
     }
     
     
 }
 
 extension SearchListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Segue to detailVC
+    }
 }
