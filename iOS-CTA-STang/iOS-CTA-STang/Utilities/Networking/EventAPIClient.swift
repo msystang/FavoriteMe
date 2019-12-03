9a01db6270b22a4f0c811938da14f4720b0c966b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 15:16:22 -0500

    creates EventAPIClient class

--- iOS-CTA-STang/iOS-CTA-STang/Utilities/Networking/EventAPIClient.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Utilities/Networking/EventAPIClient.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class EventAPIClient {
+    
+    // MARK: - Static Properties
+    
+    static let manager = EventAPIClient()
+    
+    // MARK: - Instance Methods
+    
+    static func getSearchResultsURLStr(from searchString: String) -> String {
+        return "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=\(Secrets.ticketmasterAPIKey)&keyword=\(searchString)"
+    }
+    
+    func getEvent(urlStr: String, completionHandler: @escaping (Result<[Event], AppError>) -> ())  {
+        
+        guard let url = URL(string: urlStr) else {
+            print(AppError.badURL)
+            return
+        }
+        
+        NetworkManager.manager.performDataTask(withUrl: url, andMethod: .get) { (results) in
+            switch results {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {
+                    let eventInfo = try TicketmasterResponse.decodeEventsFromData(from: data)
+                    completionHandler(.success(eventInfo))
+                }
+                catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+                
+            }
+        }
+    }
+    
+    // MARK: - Private Properties and Initializers
+    
+    private init() {}
+}
