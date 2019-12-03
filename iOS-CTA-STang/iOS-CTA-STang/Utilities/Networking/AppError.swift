88e3a1f5d0e20a12fb3cb49cd149c883eac2b642
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 15:15:42 -0500

    creates AppError enum file

---
+++ iOS-CTA-STang/iOS-CTA-STang/Utilities/Networking/AppError.swift
@@ -0,0 +1,21 @@
+//
+//  AppError.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/3/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case unauthenticated
+    case invalidJSONResponse
+    case couldNotParseJSON(rawError: Error)
+    case noInternetConnection
+    case badURL
+    case badStatusCode
+    case noDataReceived
+    case notAnImage
+    case other(rawError: Error)
+}
