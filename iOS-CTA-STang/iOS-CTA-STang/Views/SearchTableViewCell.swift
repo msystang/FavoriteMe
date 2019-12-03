cc9ea8e8eca5334a57cc074af76ddf9bfaaf0a77
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-03 16:20:10 -0500

    adds titleLabel and constraints to SearchTableViewCell

--- iOS-CTA-STang/iOS-CTA-STang/Views/SearchTableViewCell.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Views/SearchTableViewCell.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class SearchSTableViewCell: UITableViewCell {
+class SearchTableViewCell: UITableViewCell {
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        addsubViews()
+        addConstraints()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
 
+    private func addsubViews() {
+        self.contentView.addSubview(titleLabel)
+    }
+    
+    private func addConstraints() {
+        setTitleLabelConstraints()
+    }
+    
+    private func setTitleLabelConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
 }
