deb7eab657534a2a5a5818865d8a8046c10896b3
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-04 13:39:42 -0500

    creates file for MuseumItemDetail model

---
+++ iOS-CTA-STang/iOS-CTA-STang/Models/MuseumItemDetail.swift
@@ -0,0 +1,13 @@
+//
+//  MuseumItemDetail.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/4/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct MuseumItemDetail: Codable {
+    //URL to use: https://www.rijksmuseum.nl/api/en/collection/\(id)?key=\(Secrets.rijksmuseumAPIKey)
+}
