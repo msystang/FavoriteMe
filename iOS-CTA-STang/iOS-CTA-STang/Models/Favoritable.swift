b6f58aaf0457e8c39cced7865dd5468abd37a28e
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-04 12:41:19 -0500

    creates Favoritable protocol

--- iOS-CTA-STang/iOS-CTA-STang/Models/Favoritable.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Models/Favoritable.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+public protocol Favoritable {
+    var id: String { get }
+    var photoUrl: String { get }
+    
+    //Event properties
+    var isEvent: Bool? { get }
+    var eventName: String? { get }
+    var eventTimeDate: String? { get }
+    var eventPrice: String? { get }
+    var eventLink: String? { get }
+    
+    //MuseumItem properties
+    var isMuseumItem: Bool? { get }
+    var itemName: String? { get }
+    var itemDescription: String? { get }
+    var itemDateCreated: String? { get }
+    var itemPlaceProduced: String? { get }
+    
+}
