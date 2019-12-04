808505f2e20cb5058e35b2817158418ee3234cd5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-04 15:08:31 -0500

    adds functionality to signOutButton in FavoritesVC

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/Favorites/FavoritesViewController.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/Favorites/FavoritesViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
     
+    //MARK: - UI Objects
     lazy var favoritesTableView: UITableView = {
         let tableView = UITableView()
         tableView.backgroundColor = .clear
@@ -18,15 +19,18 @@ class FavoritesViewController: UIViewController {
     }()
     
     lazy var signOutButton: UIBarButtonItem = {
-        let barButton = UIBarButtonItem()
-        barButton.image = UIImage(systemName: "person.crop.circle.fill.badge.xmark")
-        //TODO: Add target action
+        let signOutImage: UIImage? = UIImage(systemName: "person.crop.circle.fill.badge.xmark")
+        let barButton = UIBarButtonItem(image: signOutImage, style: .plain, target: self, action: #selector(signOutButtonPressed))
         return barButton
     }()
     
+    //MARK: Internal Properties
+    //TODO: Declare these somewhere globally to use across VC's?
     let unfavoritedButtonImage: UIImage? = UIImage(systemName: "heart")
     let favoritedButtonImage: UIImage? = UIImage(systemName: "heart.fill")
     
+    
+    //MARK: - Lifecycle Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,6 +41,30 @@ class FavoritesViewController: UIViewController {
         
         addSubviews()
         addConstraints()
+    }
+    
+    //MARK: - Objc Functions
+    @objc func signOutButtonPressed() {
+        FirebaseAuthService.manager.signOutUser { (result) in
+            switch result {
+            case .failure(let error):
+                print("Could not sign out:\(error)")
+            case .success(()):
+                print("Signed out")
+                self.goToLogInVC()
+            }
+        }
+    }
+    
+    //MARK: - Private Methods
+    private func goToLogInVC() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
+            else { return }
+        
+        UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromTop, animations: {
+                window.rootViewController = LogInViewController()
+        })
     }
 
 }
