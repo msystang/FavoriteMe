971ff031f2680799fe122c0604cfdd31ad518fe1
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-05 17:29:41 -0500

    changes favoriteButtonImage whwhen pressed in SearchListVC

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/SearchListViewController/SearchListViewController+FavoriteButtonDelegate.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/SearchListViewController/SearchListViewController+FavoriteButtonDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension SearchListViewController: FavoriteButtonDelegate {
     //MARK: - SearchCellDelegate Functions
@@ -20,14 +21,20 @@ extension SearchListViewController: FavoriteButtonDelegate {
                 case .failure(let error):
                     print(error)
                 case .success(let isFavoritedInFB):
+                    let indexPath = IndexPath.init(row: tag, section: 0)
+                    let cell = self?.searchTableView.cellForRow(at: indexPath) as! SearchTableViewCell
                     
                     switch isFavoritedInFB {
                     case false:
                         self?.saveFavoritableToFirestore(favoritableObject: favoritableObject)
+                        //TODO: Refactor and add image setting in different func
+                        cell.favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
                     case true:
                         self?.deleteFavoritableFromFirebase(favoritableObject: favoritableObject)
+                        cell.favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
                     }
                     self?.changeButtonAppearance(favoritableObject: favoritableObject, isFavoritedInFB: isFavoritedInFB)
+                    
                 }
             }
         }
