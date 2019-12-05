1e3fe08289f2cda2bbf8e163f86fc41e477340cc
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-05 14:04:25 -0500

    adds functionality to checkExistsInFavorites func and saveFavoritableToFirestore func in SearchListVC+SearchCellDelegate

--- iOS-CTA-STang/iOS-CTA-STang/Controllers/Search/SearchListViewController/SearchListViewController+SearchCellDelegate.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/Search/SearchListViewController/SearchListViewController+SearchCellDelegate.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+
+extension SearchListViewController: SearchCellDelegate {
+    //MARK: - SearchCellDelegate Functions
+    func checkExistsInFavorites(tag: Int) {
+        //Check if already in favorites
+        let favoritableObject = favoritableObjects[tag]
+        var isFavorited: Bool = false
+        
+        favoritableObject.existsInFavorites(userID: currentUser.uid) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let isFavoritedFromFB):
+                isFavorited = isFavoritedFromFB
+                switch isFavorited {
+                case false:
+                    self?.saveFavoritableToFirestore(favoritableObject: favoritableObject)
+                case true:
+                    self?.deleteFavoritableFromFirebase(favoritableObject: favoritableObject)
+                }
+            }
+        }
+    }
+    
+    //TODO: Determine if these functions need to be in protocol
+    func saveFavoritableToFirestore(favoritableObject: Favoritable) {
+        let newFavorite = Favorite(creatorID: currentUser.uid, favoritableObject: favoritableObject)
+        
+        FirestoreService.manager.storeFavorite(favorite: newFavorite) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(()):
+                print("Saved new favorite to Firestore")
+                self?.changeButtonAppearance()
+            }
+        }
+    }
+    
+    func deleteFavoritableFromFirebase(favoritableObject: Favoritable) {
+        
+    }
+    
+    func changeButtonAppearance() {
+        //Make this property somewhere more global?
+//        switch isFavorited {
+//        case true:
+//            //Change img
+//            isFavorited = false
+//        case false:
+//            //Change img
+//            isFavorited = true
+//        }
+    }
+
+    
+}
