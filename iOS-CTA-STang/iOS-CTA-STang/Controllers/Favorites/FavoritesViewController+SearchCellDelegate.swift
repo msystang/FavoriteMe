513af19914a9ec9fad500df8cb8bbe104b3810b7
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-05 16:05:19 -0500

    conforms FavoritesVC to Search Cell Delegate

---
+++ iOS-CTA-STang/iOS-CTA-STang/Controllers/Favorites/FavoritesViewController+SearchCellDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  FavoritesViewController+SearchCellDelegate.swift
+//  iOS-CTA-STang
+//
+//  Created by Sunni Tang on 12/5/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+extension FavoritesViewController: SearchCellDelegate {
+    //MARK: - SearchCellDelegate Functions
+    func checkExistsInFavorites(tag: Int) {
+        let favorite = favorites[tag]
+        let objectID = favorite.objectID
+        
+        deleteFavoritableFromFirebase(objectID: objectID)
+    }
+    
+    func deleteFavoritableFromFirebase(objectID: String) {
+        
+        FirestoreService.manager.deleteFavorite(forUserID: FirebaseAuthService.manager.currentUser!.uid, objectID: objectID) { (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(()):
+                print("Removed from favorites")
+            }
+        }
+    }
+    
+    func changeButtonAppearance() {
+        //Make this property somewhere more global?
+        //        switch isFavorited {
+        //        case true:
+        //            //Change img
+        //            isFavorited = false
+        //        case false:
+        //            //Change img
+        //            isFavorited = true
+        //        }
+    }
+    
+    
+}
