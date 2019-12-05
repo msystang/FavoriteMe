d8f1d819546eb4242f752538566cdf51bc8e6818
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-05 10:57:05 -0500

    creates MuseumItemDetail model

--- iOS-CTA-STang/iOS-CTA-STang/Models/MuseumItemDetail.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Models/MuseumItemDetail.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+struct MuseumItemDetailWrapper: Codable {
+    let artObject: MuseumItemDetail
+    
+    static func decodeMuseumItemDetailsFromData(from jsonData: Data) throws -> MuseumItemDetail {
+    let response = try JSONDecoder().decode(MuseumItemDetailWrapper.self, from: jsonData)
+        return response.artObject
+    }
+}
+
 struct MuseumItemDetail: Codable {
     //URL to use: https://www.rijksmuseum.nl/api/en/collection/\(objectNumber)?key=\(Secrets.rijksmuseumAPIKey)
+    
+    let objectNumber: String
+    let plaqueDescriptionEnglish: String
+    
 }
