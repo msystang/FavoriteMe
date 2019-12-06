be9097aa6e20f479c874f52a46538ac15ac2fe64
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-06 13:48:23 -0500

    refactors searchString in MuseumItemAPIClient to get urlStr

--- iOS-CTA-STang/iOS-CTA-STang/Utilities/Networking/APIClients/MuseumItemAPIClient.swift
+++ iOS-CTA-STang/iOS-CTA-STang/Utilities/Networking/APIClients/MuseumItemAPIClient.swift
@@ -17,7 +17,8 @@ class MuseumItemAPIClient {
     //MARK: - Instance Methods
     
     static func getSearchResultsURLStr(from searchString: String) -> String {
-        return "https://www.rijksmuseum.nl/api/en/collection?key=\(Secrets.rijksmuseumAPIKey)&format=json&q=\(searchString)&ps=20"
+        let formattedSearchString = searchString.replacingOccurrences(of: " ", with: "%20")
+        return "https://www.rijksmuseum.nl/api/en/collection?key=\(Secrets.rijksmuseumAPIKey)&format=json&q=\(formattedSearchString)&ps=20"
     }
     
     func getMuseumItem(urlStr: String, completionHandler: @escaping (Result<[MuseumItem], AppError>) -> ())  {
